47f9f74b36545167316c174c669f4229364b67cd
h0t_R0d <rodrigo@plasticitylabs.com>
2018-05-22 12:00:39 -0400

    Test github repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='daveBot',
+      version='0.1',
+      description='Plasticity Natural Language Understanding library',
+      url='http://github.com/sineco/daveBot',
+      author='Rodrigo Araujo',
+      author_email='rodrigo@plasticitylabs.com',
+      license='',
+      packages=['daveBot'],
+      zip_safe=False)
